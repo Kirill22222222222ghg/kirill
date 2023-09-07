57d9750586e7b98080666bd7ff2cba22a488f644
Kirill <efrem02004@yandex,ru>
2023-09-07 15:51:43 +0300

    afafaf

--- main.c
+++ main.c
@@ -6,7 +6,7 @@ int main(int argc, char **argv)
     char first[255], last[255];
 
     printf("Enter your first name: ");
-    fgets(first, 255, stdin);
+    fgets(first, 255, stdin);affafaf
     first[strlen(first)-1] = '\0'; /* remove the newline at the end */
 
     printf("Now enter your last name: ");
