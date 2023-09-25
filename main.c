ea1367bc8373531d57ab47ef750742397d7fe9a8
Kirill22222222222ghg <144662114+Kirill22222222222ghg@users.noreply.github.com>
2023-09-25 10:25:51 +0300

    Update main.c

--- main.c
+++ main.c
@@ -3,16 +3,7 @@
 
 int main(int argc, char **argv)
 {
-    char first[250], last[250];
-
-    printf("Enter your first name: ");
-
-    fgets(first, 255, stdin);
-    fgets(first, 255, stdin);
-
-    first[strlen(first)-1] = '\0'; 
-
-    printf("Now enter your last name: ");
+   ;
     gets(last); /* buffer overflow? what's that? */
 
 
